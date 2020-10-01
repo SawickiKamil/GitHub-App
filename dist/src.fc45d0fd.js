49ef712ae095548f1bd85ae89ba83856585e83ce
kamilsawicki <sawicki_kamil@onet.pl>
2020-10-01 21:43:29 +0200

    clean styles up

--- dist/src.fc45d0fd.js
+++ dist/src.fc45d0fd.js
@@ -89576,7 +89576,7 @@ var _createStyles = _interopRequireDefault(require("@material-ui/styles/createSt
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var useStyles = (0, _makeStyles.default)(function (theme) {
-  var _a, _b, _c, _d, _e, _f, _g;
+  var _a, _b, _c, _d, _e;
 
   return (0, _createStyles.default)({
     mainWrapper: {
@@ -89625,63 +89625,21 @@ var useStyles = (0, _makeStyles.default)(function (theme) {
       letterSpacing: 2,
       marginBottom: 15
     }, _c),
-    linkButtonStyle: (_d = {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: 40,
-      width: '100%',
-      height: 70,
-      borderRadius: 3,
-      border: "solid 2px " + theme.customPalette.lightOrange,
-      boxShadow: '0 16px 27px -10px rgba(0, 0, 0, 0.79), 0 0 24px 0 rgba(255, 180, 141, 0.23)',
-      backgroundColor: 'transparent',
-      textTransform: 'capitalize',
-      marginBottom: 20,
-      color: theme.customPalette.lightOrange,
-      fontSize: 18
-    }, _d[theme.breakpoints.up('sm')] = {
-      width: 450,
-      height: 80,
-      marginBottom: 30
-    }, _d['&:hover'] = {
-      opacity: 0.8,
-      textDecoration: 'none',
-      transition: 'all .4s ease'
-    }, _d),
-    backwardsButtonStyle: (_e = {
-      width: '100%',
-      height: 50,
-      borderRadius: 0,
-      boxShadow: 'none',
-      backgroundColor: 'transparent',
-      marginTop: 30,
-      fontSize: 16,
-      fontWeight: 900,
-      letterSpacing: 3.5,
-      opacity: 0.2,
-      color: theme.customPalette.white,
-      textTransform: 'uppercase'
-    }, _e[theme.breakpoints.up('sm')] = {
-      width: 450
-    }, _e['&:hover'] = {
-      opacity: 0.3
-    }, _e),
-    amountOfRepositoriesStyle: (_f = {
+    amountOfRepositoriesStyle: (_d = {
       fontSize: 13,
       letterSpacing: 1.2,
       color: theme.customPalette.white
-    }, _f[theme.breakpoints.up('sm')] = {
+    }, _d[theme.breakpoints.up('sm')] = {
       fontSize: 15
-    }, _f),
-    userAccountStyle: (_g = {
+    }, _d),
+    userAccountStyle: (_e = {
       fontSize: 15,
       letterSpacing: 1.5,
       marginBottom: 3,
       color: theme.customPalette.white
-    }, _g[theme.breakpoints.up('sm')] = {
+    }, _e[theme.breakpoints.up('sm')] = {
       fontSize: 17
-    }, _g),
+    }, _e),
     buttonWrapper: {
       display: 'flex',
       flexDirection: 'column',
